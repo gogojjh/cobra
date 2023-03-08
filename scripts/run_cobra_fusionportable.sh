35cd199ca89fb86281aa9a5362a1330df7141fe2
gogojjh <jiaojh1994@gmail.com>
2023-03-08 11:11:52 +0800

    add submodule: rviz_plugins, rviz_plugin_tutorials

--- scripts/run_cobra_fusionportable.sh
+++ scripts/run_cobra_fusionportable.sh
@@ -1,3 +1,6 @@
+# Publish camera info
+rosrun cobra_tools publish_senosr_info /Titan/dataset/FusionPortable_dataset/calibration_files/20220209_calib/calib
+
 # Run pointcloud_to_image
 roslaunch pointcloud_image_converter pc2img_fusionportable.launch &
 
